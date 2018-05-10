366dbc4de8fc92cdba1053affe677b1f6cdf4cae
Ilya Dolmatov <dolmatoffilya@gmail.com>
2018-05-10 13:13:31 +0300

    little change logic

--- api/models/Dish.js
+++ api/models/Dish.js
@@ -7,58 +7,58 @@
 
 module.exports = {
 
-  attributes: {
+    attributes: {
 
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+        //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+        //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+        //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
 
-    additionalInfo: {type: 'string'},
-    code: {type: 'string'},
-    description: {type: 'string'},
-    id: {type: 'string'},
-    isDeleted: {type: 'string'},
-    name: {type: 'string'},
-    seoDescription: {type: 'string'},
-    seoKeywords: {type: 'string'},
-    seoText: {type: 'string'},
-    seoTitle: {type: 'string'},
-    tags: {type: 'string'},
-    carbohydrateAmount: {type: 'number'},
-    carbohydrateFullAmount: {type: 'number'},
-    differentPricesOn: {type: 'array'},
-    doNotPrintInCheque: {type: 'boolean'},
-    energyAmount: {type: 'number'},
-    energyFullAmount: {type: 'number'},
-    fatAmount: {type: 'number'},
-    fatFullAmount: {type: 'number'},
-    fiberAmount: {type: 'number'},
-    fiberFullAmount: {type: 'number'},
-    groupId: {type: 'string'},
-    groupModifiers: {type: 'array'},
-    measureUnit: {type: 'string'},
-    modifiers: {type: 'array'},
-    price: {type: 'number'},
-    productCategoryId: {type: 'string'},
-    prohibitedToSaleOn: {type: 'array'},
-    type: {type: 'string'},
-    useBalanceForSell: {type: 'boolean'},
-    weight: {type: 'number'},
-    images: {type: 'array'},
-    isIncludedInMenu: {type: 'boolean'},
-    order: {type: 'number'},
-    parentGroup: {type: 'string'}
+        additionalInfo: {type: 'string'},
+        code: {type: 'string'},
+        description: {type: 'string'},
+        id: {type: 'string'},
+        isDeleted: {type: 'string'},
+        name: {type: 'string'},
+        seoDescription: {type: 'string'},
+        seoKeywords: {type: 'string'},
+        seoText: {type: 'string'},
+        seoTitle: {type: 'string'},
+        tags: {type: 'string'},
+        carbohydrateAmount: {type: 'number'},
+        carbohydrateFullAmount: {type: 'number'},
+        differentPricesOn: {type: 'array'},
+        doNotPrintInCheque: {type: 'boolean'},
+        energyAmount: {type: 'number'},
+        energyFullAmount: {type: 'number'},
+        fatAmount: {type: 'number'},
+        fatFullAmount: {type: 'number'},
+        fiberAmount: {type: 'number'},
+        fiberFullAmount: {type: 'number'},
+        groupId: {type: 'string'},
+        groupModifiers: {type: 'array'},
+        measureUnit: {type: 'string'},
+        modifiers: {type: 'array'},
+        price: {type: 'number'},
+        productCategoryId: {type: 'string'},
+        prohibitedToSaleOn: {type: 'array'},
+        type: {type: 'string'},
+        useBalanceForSell: {type: 'boolean'},
+        weight: {type: 'number'},
+        images: {type: 'array'},
+        isIncludedInMenu: {type: 'boolean'},
+        order: {type: 'number'},
+        parentGroup: {type: 'string'}
 
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+        //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+        //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+        //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
 
 
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+        //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+        //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+        //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
 
-  },
+    },
 
 };
 
