7cc5b599586e3e3a5f6a32432e623c07bc3c8d9d
Ilya <dolmatoffilya@gmail.com>
2019-03-14 21:11:58 +0200

    fix timezone rename deliveryActions on actions add causes

--- models/Condition.js
+++ models/Condition.js
@@ -11,7 +11,8 @@
  * @apiParam {JSON} actions Объект действий, которые выполняются при выполнении всех условий
  */
 
-const deliveryActions = require('../lib/deliveryActions');
+const actions = require('../lib/actions');
+const causes = require('../lib/causes');
 
 module.exports = {
   attributes: {
@@ -31,26 +32,8 @@ module.exports = {
       collection: 'zone'
     },
 
-    check: function (cart) {
-      return new Promise(async (resolve, reject) => {
-        if (!this.enable)
-          resolve(false);
-
-        if (!this.causes)
-          resolve(true);
-
-        if (!checkTime(this.causes.workTime))
-          resolve(false);
-
-        cart.count(cart, err => {
-          if (err) return reject(err);
-
-          if (this.causes.cartAmount)
-            resolve(between(this.causes.cartAmount.valueFrom, this.causes.cartAmount.valueTo, cart.cartTotal));
-
-          resolve(true);
-        });
-      });
+    check: async function (cart) {
+      return await causes(cart);
     },
 
     exec: function (cart) {
@@ -74,7 +57,7 @@ module.exports = {
   },
 
   action: async function (actionName, params) {
-    const action = deliveryActions[actionName];
+    const action = actions[actionName];
     if (!action) {
       throw 'action not found'
     }
@@ -82,52 +65,3 @@ module.exports = {
     return await action(params);
   }
 };
-
-function checkTime(timeArray) {
-  if (!timeArray || !timeArray.length)
-    return true;
-
-  const weekday = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-
-  const date = new Date();
-
-  function checkHours(start, stop) {
-    try {
-      start = start.split(':');
-    } catch (e) {
-    }
-    try {
-      stop = stop.split(':');
-    } catch (e) {
-    }
-    const sh = start ? start[0] : start;
-    const sm = start ? start[1] : start;
-    const eh = stop ? stop[0] : stop;
-    const em = stop ? stop[1] : stop;
-    sails.log.info(between(sh * 60 + sm, eh * 60 + em, date.getHours() * 60 + date.getMinutes()));
-    return between(sh * 60 + sm, eh * 60 + em, date.getHours() * 60 + date.getMinutes());
-  }
-
-  try {
-    const all = timeArray.filter(t => t.dayOfWeek === 'all')[0];
-    if (all) {
-      return checkHours(all.start, all.stop)
-    }
-
-    const today = weekday[date.getDay()];
-    const day = timeArray.filter(t => t.dayOfWeek.toLowerCase() === today)[0];
-
-    if (day) {
-      return checkHours(day.start, day.stop);
-    }
-
-    return timeArray.length === 0;
-  } catch (e) {
-    sails.log.error(e);
-    return false;
-  }
-}
-
-function between(from, to, a) {
-  return ((!from && !to) || (!from && to > a) || (!to && from < a) || (from < a && to > a));
-}
