d125a0e6c96cd5c92501924e649259867cf5a6f4
Siziy Ivan <cziy@ya.ru>
2020-10-27 09:24:21 +0300

    Fix children in getGroup

--- models/Group.js
+++ models/Group.js
@@ -132,7 +132,7 @@ module.exports = {
                         }
                     });
                     delete menu[group.id].childGroups;
-                    menu[group.id].children = [];
+                    menu[group.id].children = childGroups;
                     if (menu[group.id].children.length > 1)
                         menu[group.id].children.sort((a, b) => a.order - b.order);
                 }
