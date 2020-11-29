0683962ce893424953b59f382bbb5f4873b23832
Siziy Ivan <cziy@ya.ru>
2020-11-29 16:09:57 +0300

    todo rename

--- adapter/image/Image.js
+++ adapter/image/Image.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Image {
     constructor(url, name, key) {
         this.url = url;
+        // TODO: переименовать нейм по идеи это размеры картинок + оригинал (передается оно в images)
         this.name = name;
         this.key = key;
     }
--- adapter/image/Image.ts
+++ adapter/image/Image.ts
@@ -10,6 +10,7 @@ export default class Image {
 
   constructor(url: string, name: { origin: string }, key: string) {
     this.url = url;
+    // TODO: переименовать нейм по идеи это размеры картинок + оригинал (передается оно в images)
     this.name = name;
     this.key = key;
   }
