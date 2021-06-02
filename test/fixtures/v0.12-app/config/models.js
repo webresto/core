9f579b7c9469fde958408bd1439be04cbfc6d51b
Siziy Ivan <cziy@ya.ru>
2021-06-02 12:23:30 +0300

    model fixture drop before test

--- test/fixtures/v0.12-app/config/models.js
+++ test/fixtures/v0.12-app/config/models.js
@@ -1,15 +1,3 @@
-
-var migrate;
-if (process.env.DB_MIGRATE  === 'alter' || process.env.DB_MIGRATE  === 'safe' || process.env.DB_MIGRATE  === 'drop'){
-  migrate = process.env.DB_MIGRATE
-} else {
-  migrate = 'safe'
-}
-
-
 module.exports.models = {
-    migrate: migrate  
-  // migrate: 'drop'
-  // migrate: 'safe'
-//    migrate: 'alter'
+   migrate: 'drop'
 };
