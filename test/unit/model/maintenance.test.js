1ae02b9f33e0bc9086de2fce312fb5179623775c
Siziy Ivan <cziy@ya.ru>
2021-06-02 13:11:14 +0300

    delete test.js d.ts

--- test/unit/model/maintenance.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-describe('Maintenance', function () {
-    it('siteIsOff TODO', async function () {
-    });
-    it('TODO', async function () {
-        //TODO
-    });
-});
