37f5aa30cb0880ae068e8791fcb757495cfb5927
Siziy Ivan <cziy@ya.ru>
2021-07-16 19:44:49 +0300

    adapter adapted for factory

--- adapter/index.ts
+++ adapter/index.ts
@@ -1,20 +1,29 @@
-import RMSAdapter from './rms/RMSAdapter';
-import MapAdapter from './map/MapAdapter';
+import RMSAdapter from "./rms/RMSAdapter";
+import MapAdapter from "./map/MapAdapter";
 import ImageAdapter from "./image/ImageAdapter";
-import PaymentAdapter from './payment/PaymentAdapter';
-
+import PaymentAdapter from "./payment/PaymentAdapter";
+import * as fs from "fs";
+const WEBRESTO_MODULES_PATH =
+  process.env.WEBRESTO_MODULES_PATH === undefined
+    ? "@webresto"
+    : process.env.WEBRESTO_MODULES_PATH;
 /**
  * Отдаёт запрашиваемый RMS-адаптер
  */
 export class RMS {
   public static getAdapter(adapterName: string): typeof RMSAdapter {
-    const adapterFind = '@webresto/' + adapterName.toLowerCase() + '-rms-adapter';
+    // For factory we use different dirrectory for modules
+    let adapterLocation =
+      WEBRESTO_MODULES_PATH + "/" + adapterName.toLowerCase() + "-rms-adapter";
+    adapterLocation = fs.existsSync(adapterLocation)
+      ? adapterLocation
+      : "@webresto/" + adapterName.toLowerCase() + "-rms-adapter";
     try {
-      const adapter = require(adapterFind);
+      const adapter = require(adapterLocation);
       return adapter.RMS[adapterName.toUpperCase()];
     } catch (e) {
       sails.log.error("CORE > getAdapter RMS > error; ", e);
-      throw new Error('Module ' + adapterFind + ' not found');
+      throw new Error("Module " + adapterLocation + " not found");
     }
   }
 }
@@ -24,13 +33,19 @@ export class RMS {
  */
 export class Map {
   public static getAdapter(adapterName: string): new (config) => MapAdapter {
-    adapterName = '@webresto/' + adapterName.toLowerCase() + '-map-adapter';
+    let adapterLocation =
+      WEBRESTO_MODULES_PATH + "/" + adapterName.toLowerCase() + "-map-adapter";
+    adapterLocation = fs.existsSync(adapterLocation)
+      ? adapterLocation
+      : "@webresto/" + adapterName.toLowerCase() + "-map-adapter";
     try {
-      const adapter: { MapAdapter: { default: new (config) => MapAdapter } } = require(adapterName);
+      const adapter: {
+        MapAdapter: { default: new (config) => MapAdapter };
+      } = require(adapterLocation);
       return adapter.MapAdapter.default;
     } catch (e) {
       sails.log.error("CORE > getAdapter Map > error; ", e);
-      throw new Error('Module ' + adapterName + ' not found');
+      throw new Error("Module " + adapterLocation + " not found");
     }
   }
 }
@@ -40,28 +55,43 @@ export class Map {
  */
 export class ImageA {
   public static getAdapter(adapterName: string): ImageAdapter {
-    adapterName = '@webresto/' + adapterName.toLowerCase() + '-image-adapter';
+    let adapterLocation =
+      WEBRESTO_MODULES_PATH +
+      "/" +
+      adapterName.toLowerCase() +
+      "-image-adapter";
+    adapterLocation = fs.existsSync(adapterLocation)
+      ? adapterLocation
+      : "@webresto/" + adapterName.toLowerCase() + "-image-adapter";
     try {
-      const adapter = require(adapterName);
+      const adapter = require(adapterLocation);
       return adapter.ImageAdapter.default;
     } catch (e) {
       sails.log.error("CORE > getAdapter ImageA > error; ", e);
-      throw new Error('Module ' + adapterName + ' not found');
+      throw new Error("Module " + adapterLocation + " not found");
     }
   }
 }
-  /**
+/**
  * Отдаёт запрашиваемый Payment-адаптер
  */
 export class Payment {
   public static getAdapter(adapterName: string): PaymentAdapter {
-    adapterName = '@webresto/' + adapterName.toLowerCase() + '-payment-adapter';
+    let adapterLocation =
+      WEBRESTO_MODULES_PATH +
+      "/" +
+      adapterName.toLowerCase() +
+      "-payment-adapter";
+    adapterLocation = fs.existsSync(adapterLocation)
+      ? adapterLocation
+      : "@webresto/" + adapterName.toLowerCase() + "-payment-adapter";
+
     try {
-      const adapter = require(adapterName);
+      const adapter = require(adapterLocation);
       return adapter.PaymentAdapter[adapterName.toUpperCase()];
     } catch (e) {
       sails.log.error("CORE > getAdapter Payment > error; ", e);
-      throw new Error('Module ' + adapterName + ' not found');
+      throw new Error("Module " + adapterLocation + " not found");
     }
   }
 }
