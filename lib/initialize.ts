576aed63c06a014ad835a9ae403af6f681359051
Siziy Ivan <cziy@ya.ru>
2021-08-21 09:48:42 +0300

    initialize stateflow 1x

--- lib/initialize.ts
+++ lib/initialize.ts
@@ -1,82 +1,41 @@
 import HookTools from "./hookTools";
 import {resolve} from "path";
 import afterHook from "./afterHook";
+import * as _ from "lodash";
 
-/**
- * SET BLUEBIRD AS GLOBAL PROMISE
- * Use @types/bluebird-global in devDep
- */
-import * as Bluebird from 'bluebird'
-global.Promise = <any>Bluebird;
-
-const State = require('sails-hook-stateflow').State;
-
-/**
- * Initialize hook. If sails.config.restocore not exists hook will not be loaded.
- * Bind models.
- * @param sails
- * @constructor
- */
 export default function ToInitialize(sails) {
 
   /**
-   * List of hooks that required
+   * Required hooks
    */
   const requiredHooks = [
     'blueprints',
     'http',
     'orm',
-    'policies'
+    'policies',
+    'stateflow'
   ];
 
   return function initialize(cb) {
-    /**
-     * Small security fixes
-     */
+  
+    // Disable blueprints magic
     if (process.env.BLUEPRINTS_SECURITY_OFF !== "TRUE") {
-      sails.log.info("Security > blueprints rest/shortcuts is OFF ");
-      //@ts-ignore
+      sails.log.info("Blueprints rest/shortcuts magic is OFF");
       sails.config.blueprints.shortcuts = false;
-      //@ts-ignore
       sails.config.blueprints.rest = false;
     }
+
+    if (sails.config.restocore.stateflow)
+      sails.config.stateflow = _.merge(sails.config.stateflow, sails.config.restocore.stateflow)
 
     /**
      * AFTER OTHERS HOOKS
      */
     HookTools.waitForHooks('restocore', requiredHooks, afterHook);
 
-    /**
-     * Set cart states in sails-hook-stateflow
-     * Эта штука не работает =) Дало заказать  
-     */
-    sails.stateflow = [
-      new State('CART', ['CHECKOUT'], function (cb) {
-        cb(null, true);
-      }),
-      new State('CHECKOUT', ['ORDER', 'PAYMENT', 'CART'], function (cb) {
-        /**
-         * CHECKOUT -> ORDER если тип платежной "promise", или отсутствует
-         * CHECKOUT -> CART  В любой момент (без условий)
-         * CHECKOUT -> PAYMENT  Если тип платежной системы "ixternal" || "internal". 
-         */
-        cb(null, true);
-      }),
-      new State('PAYMENT', ['ORDER', 'CHECKOUT'], function (cb) {
-        /**
-         * PAYMENT -> ORDER при успешной оплате.
-         * PAYMENT -> CHECKOUT при неуспешной оплате
-         */
-        cb(null, true);
-      }),
-      new State('ORDER', [], function (cb) {
-        cb(null, true);
-      })
-    ];
-
-    /**
-     * MODELS
-     */
+  /**
+   * Bind models
+   */
     HookTools.bindModels(resolve(__dirname, '../models')).then(cb);
   }
 };
