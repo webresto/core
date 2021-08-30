045291d48b8572ce889d2a63cb306d3084fbb982
Siziy Ivan <cziy@ya.ru>
2021-08-30 10:34:24 +0300

    settings refactor

--- models/Settings.ts
+++ models/Settings.ts
@@ -1,19 +1,37 @@
-/**
- * @api {API} Settings Settings
- * @apiGroup Models
- * @apiDescription Системная информация (в данный момент ревизия)
- *
- * @apiParam {Integer} id ID
- * @apiParam {String} key Ключ доступа к свойству
- * @apiParam {String} value Значение свойства
- * @apiParam {String} section Секция, к которой относится свойство
- */
 import ORM from "../interfaces/ORM";
 import ORMModel from "../interfaces/ORMModel";
-import Config from "../interfaces/Config";
 
+let SettingsCollection = {
 
-let SettingsModel  =  {  
+  /**Id */
+  id: {
+    type: "number",
+    autoIncrement: true,
+  } as unknown as string,
+
+  /** Ключ доступа к свойству */
+  key: {
+    type: "string",
+    unique: true,
+    required: true
+  } as unknown as string,
+
+  /** Описание */
+  description: "string" as string,
+
+  /** Значение свойства */
+  value: "string",
+
+  /** Секция, к которой относится свойство */
+  section: "string" as string,
+  
+  /** Источника происхождения */
+  from: "string" as string,
+}
+
+type Settings = typeof SettingsCollection & ORM
+
+let Model  =  {  
   async use(config: string, key: string): Promise<Settings> {
     sails.log.silly("CORE > Settings > use: ", key, config);
     if (!key) {
@@ -82,87 +100,12 @@ let SettingsModel  =  {
   }
 } 
 
-
-
-
-
-
-type config = typeof sails.config;
-
-// /**
-//  * Описывает класс конфигурации
-//  */
-// export interface SettingsModel extends ORMModel<Settings> {
-//   /**
-//    * Отдаёт запрашиваемый ключ из запрашиваемого конфига. Если ключ, который запрашивается, отсуствует в базе, то данные
-//    * будут взяты из sails.config[config][key] и записаны в базу. При последующих запросах того же ключа будут возвращаться данные
-//    * из базы данных. Если указать только один параметр ключ, то данные будут доставаться из sails.config.restocore[key].
-//    * @param key - ключ
-//    * @return найденное значение или 0, если значение не было найдено.
-//    */
-
-//   use<T extends keyof Config>(key: T): Promise<PropType<Config, T>>;
-//   /**
-//    * Отдаёт запрашиваемый ключ из запрашиваемого конфига. Если ключ, который запрашивается, отсуствует в базе, то данные
-//    * будут взяты из sails.config[config][key] и записаны в базу. При последующих запросах того же ключа будут возвращаться данные
-//    * из базы данных. Если указать только один параметр ключ, то данные будут доставаться из sails.config.restocore[key].
-//    * @param config - конфиг откуда доставать ключ
-//    * @param key - ключ, если не указывать второй параметр, то первый будет считаться за ключ
-//    * @return найденное значение или 0, если значение не было найдено.
-//    */
-//   use<T extends keyof config[U], U extends keyof config>(
-//     config: U,
-//     key: T
-//   ): Promise<PropType<config[U], T>>;
-//   use(key: string): Promise<Settings>;
-//   use(config: string, key: string): Promise<any>;
-
-//   /**
-//    * Проверяет существует ли настройка, если не сущестует, то создаёт новую и возвращает ее. Если существует, то обновляет его значение (value)
-//    * на новые. Также при первом внесении запишется параметр (config), отвечающий за раздел настройки.
-//    * @param values
-//    * @return обновлённое или созданное блюдо
-//    */
-//   set(
-//     key: string,
-//     value: string | number | boolean,
-//     config?: string
-//   ): Promise<any>;
-// }
-
-let SettingsCollection = {
-  /**Id */
-  id: {
-    type: "number",
-    autoIncrement: true,
-  } as unknown as string,
-  key: {
-    type: "string",
-    unique: true,
-    required: true
-  } as unknown as string ,
-  description: "string" as string,
-  value: "string",
-  section: "string" as string,
-  from: "string" as string,
-}
-
-type Settings = typeof SettingsCollection
-
-/**
- * Описывает одно поле конфига, его значение, ключ и откуда оно было взято
- */
- //export default 
- 
-
 module.exports = {
   primaryKey: "id",
   attributes: SettingsCollection,
-  ...SettingsModel
+  ...Model
 }
 
-
-
 declare global {
-  const Settings: typeof SettingsModel & ORMModel<Settings>;
+  const Settings: typeof Model & ORMModel<Settings>;
 }
