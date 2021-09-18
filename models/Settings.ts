4a3091ee32abd95967269211bb819df3adb6115d
Siziy Ivan <cziy@ya.ru>
2021-09-18 09:59:54 +0300

    check logic

--- models/Settings.ts
+++ models/Settings.ts
@@ -2,7 +2,6 @@ import ORM from "../interfaces/ORM";
 import ORMModel from "../interfaces/ORMModel";
 
 let attributes = {
-
   /**Id */
   id: {
     type: "number",
@@ -13,7 +12,7 @@ let attributes = {
   key: {
     type: "string",
     unique: true,
-    required: true
+    required: true,
   } as unknown as string,
 
   /** Описание */
@@ -24,88 +23,84 @@ let attributes = {
 
   /** Секция, к которой относится свойство */
   section: "string" as string,
-  
+
   /** Источника происхождения */
   from: "string" as string,
-}
+};
 
 type attributes = typeof attributes;
 interface Settings extends attributes, ORM {}
-export default Settings
+export default Settings;
 
-let Model  =  {  
+let Model = {
   /** retrun setting value by key */
-  async use(config: string, key: string): Promise<Settings> {
-    sails.log.silly("CORE > Settings > use: ", key, config);
-    if (!key) {
-      key = config;
-      config = "restocore";
-    }
+  async use(key: string, from?: string): Promise<any> {
+    sails.log.silly("CORE > Settings > use: ", key, from);
+
+    let value: any;
 
     /** ENV variable is important*/
     if (process.env[key] !== undefined) {
       try {
-        return JSON.parse(process.env[key]);
+        value = JSON.parse(process.env[key]);
       } catch (e) {
         sails.log.error("CORE > Settings > use ENV parse error: ", e);
+        value = process.env[key];
+      } finally {
+        if (!(await Settings.find({ key: key }).limit(1))[0])
+          await Settings.set(key, value, "env");
+
+        return value;
       }
     }
 
-    let obj = (await Settings.find({ key: key }).limit(1))[0];
-    sails.log.silly("CORE > Settings > findOne: ", key, obj);
+    /** If variable present in database */
 
-    if (!obj) {
-      let value: string;
+    let setting = (await Settings.find({ key: key }).limit(1))[0];
+    sails.log.silly("CORE > Settings > findOne: ", key, setting);
+    if (setting && setting.value) return setting.value;
 
-      if (sails.config[config][key]) {
-        value = sails.config[config][key];
-      } else {
-        return undefined;
-      }
-
-      value = JSON.stringify(value);
-      try {
-        obj = await Settings.set(key, value, config);
-      } catch (e) {
-        sails.log.error(key, value, config, e);
+    /** Variable present in config */
+    if (from) {
+      if (sails.config[from] && sails.config[from][key]) {
+        value = sails.config[from][key];
+        await Settings.set(key, value, from);
+        return value;
       }
     }
 
-    let value = obj.value;
-    try {
-      value = JSON.parse(obj.value);
-    } finally {
-      return value;
-    }
+    throw `Setting with name ${key} not found`;
   },
 
   /**
    * Проверяет существует ли настройка, если не сущестует, то создаёт новую и возвращает ее. Если существует, то обновляет его значение (value)
    * на новые. Также при первом внесении запишется параметр (config), отвечающий за раздел настройки.
    */
-  async set(key: string, value: any, config?: string): Promise<any> {
+  async set(key: string, value: any, from?: string): Promise<any> {
     try {
       const propety = await Settings.findOne({ key: key });
       if (!propety) {
         return Settings.create({
           key: key,
           value: value,
-          from: config,
+          from: from,
         });
       } else {
-        return (await Settings.update({ key: key }, { value: value }).fetch())[0];
+        return (
+          await Settings.update({ key: key }, { value: value }).fetch()
+        )[0];
       }
     } catch (e) {
-      sails.log.error("CORE > Settings > set: ", key, value, config, e);
+      sails.log.error("CORE > Settings > set: ", key, value, from, e);
     }
-  }
-} 
+  },
+};
 
 module.exports = {
   primaryKey: "id",
   attributes: attributes,
-  ...Model
-}
+  ...Model,
+};
 
 declare global {
   const Settings: typeof Model & ORMModel<Settings>;
