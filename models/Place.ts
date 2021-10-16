c6645fc3e60e63ce6ccc7eed19a2c0dceb5d8ed2
Siziy Ivan <cziy@ya.ru>
2021-10-16 15:34:28 +0300

    add place model

---
+++ models/Place.ts
@@ -0,0 +1,47 @@
+import ORM from "../interfaces/ORM";
+import ORMModel from "../interfaces/ORMModel";
+import { v4 as uuid } from "uuid";
+import { WorkTime } from "@webresto/worktime";
+
+let attributes = {
+  id: "string",
+  title: 'string',
+  address: 'string',
+  order: 'integer' as unknown as number,
+  phone: 'string',
+  enable: {
+    type: 'boolean',
+    defaultsTo: true,
+    required: true
+  } as unknown as boolean,
+  worktime: 'json' as unknown as WorkTime,
+  isPickupPoint: 'boolean'as unknown as boolean,
+  isCookingPoint: 'boolean'as unknown as boolean,
+  isSalePoint: 'boolean'as unknown as boolean,
+  customData: 'json' as unknown as any
+};
+type attributes = typeof attributes;
+interface Place extends attributes, ORM {}
+export default Place;
+
+let Model = {
+  beforeCreate(placeInit: any, next: any) {
+    if (!placeInit.id) {
+      placeInit.id = uuid();
+    }
+    
+    next();
+  },
+};
+
+module.exports = {
+  primaryKey: "id",
+  attributes: attributes,
+  ...Model,
+};
+
+declare global {
+  const Place: typeof Model & ORMModel<Place>;
+}
+
+
