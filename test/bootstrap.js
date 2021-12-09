d6c05391732615ce08c7e5abf7108659531f87f6
Xziy <x@42.pub>
2021-12-09 16:57:56 +0300

    merge

---
+++ test/bootstrap.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("mocha");
+require("dotenv").config();
+var Sails = require("./fixture/node_modules/sails").Sails;
+before(function (done) {
+    let rc = require("./fixture/app-export").rc;
+    this.timeout(50000);
+    Sails().lift(rc, function (err, _sails) {
+        if (err)
+            return done(err);
+        global.sails = _sails;
+        return done();
+    });
+});
+after(function (done) {
+    if (global.sails) {
+        return global.sails.lower(function (err) {
+            if (err) {
+                done();
+            }
+            done();
+        });
+    }
+    done();
+});
