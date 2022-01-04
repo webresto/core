c3ef4c05925a31e8390869fd37b0d463265486a8
Roman Serebriakov <rserebryakov1902@gmail.com>
2022-01-04 17:34:41 +0200

    added Place.js to merge from master

---
+++ models/Place.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const uuid_1 = require("uuid");
+let attributes = {
+    id: "string",
+    title: 'string',
+    address: 'string',
+    order: 'integer',
+    phone: 'string',
+    enable: {
+        type: 'boolean',
+        defaultsTo: true,
+        required: true
+    },
+    worktime: 'json',
+    isPickupPoint: 'boolean',
+    isCookingPoint: 'boolean',
+    isSalePoint: 'boolean',
+    customData: 'json'
+};
+let Model = {
+    beforeCreate(placeInit, next) {
+        if (!placeInit.id) {
+            placeInit.id = (0, uuid_1.v4)();
+        }
+        next();
+    },
+};
+module.exports = {
+    primaryKey: "id",
+    attributes: attributes,
+    ...Model,
+};
