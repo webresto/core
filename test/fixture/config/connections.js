d49eaad71dfaa1be47a792fb5650f3d82367e45f
Xziy <x@42.pub>
2022-11-07 13:05:24 +0300

    memory  store on tests

--- test/fixture/config/connections.js
+++ test/fixture/config/connections.js
@@ -1,9 +1,6 @@
 module.exports.connections = {
-    // postgres: {
-    //     adapter: 'sails-postgresql',
-    //     host: process.env.PG_HOST === undefined ? '127.0.0.1' : process.env.PG_HOST,
-    //     user: process.env.PG_USER === undefined ? 'postgres' : process.env.PG_USER, 
-    //     password: process.env.PG_PASSWORD === undefined ? 'postgres' : process.env.PG_PASSWORD, 
-    //     database: process.env.PG_DATABASE === undefined ? 'testdb' : process.env.PG_DATABASE 
-    //   }
+    default: {
+        adapter: "sails-disk",
+        inMemoryOnly: true
+    }
 };
