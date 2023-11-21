f9ea2982a7cd0032f51cdfd099d4c82f85a8d06a
wsderte <alek81299@gmail.com>
2023-11-21 12:59:07 +0200

    pass tests

--- adapters/promotion/default/promotionAdapter.ts
+++ adapters/promotion/default/promotionAdapter.ts
@@ -55,7 +55,8 @@ export class PromotionAdapter extends AbstractPromotionAdapter {
       if (promotionByConcept[0] === undefined) return dish;
 
       // TODO: this should work on first condition isJoint and isPublic should be true
-
+      console.log(dish, "dish")
+      console.log(this.promotions[promotionByConcept[0].id].displayDish(dish), "this.promotions[promotionByConcept[0].id].displayDish(dish)")
       return this.promotions[promotionByConcept[0].id].displayDish(dish)
     } catch (error) {
 
