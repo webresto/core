e8a77f648e7d10a71129a4d9d521be3136177e9d
Xziy <xziy88@gmail.com>
2023-11-25 17:32:07 +0300

    modifier limit find instead findOne

--- models/Dish.js
+++ models/Dish.js
@@ -273,7 +273,7 @@ let Model = {
                     else {
                         throw `Group modifierId or rmsId not found`;
                     }
-                    dish.modifiers[index].group = await Group.findOne(criteria);
+                    dish.modifiers[index].group = await Group.find(criteria).limit(1)[0];
                 }
                 if (!modifier.childModifiers)
                     modifier.childModifiers = [];
@@ -290,7 +290,7 @@ let Model = {
                     else {
                         throw `Dish modifierId or rmsId not found`;
                     }
-                    let childModifierDish = await Dish.findOne(criteria).populate('images');
+                    let childModifierDish = await Dish.find(criteria).populate('images').limit(1)[0];
                     if (!childModifierDish || (childModifierDish && childModifierDish.balance === 0)) {
                         // delete if dish not found
                         sails.log.warn("DISH > getDishModifiers: Modifier " + childModifier.modifierId + " from dish:" + dish.name + " not found");
