2b54a070e689761957c36ca18c24681a64d800c0
VuXfi <x@42.pub>
2025-10-06 15:52:38 +0700

    refactor: remove obsolete ProductCatalog and ProductMediaManager definitions to streamline codebase

--- libs/adminpanel/ProductMediaManager/helpers/ConvertType.d.ts
+++
@@ -1,8 +0,0 @@
-import { MediaManagerItem } from "sails-adminpanel/lib/media-manager/AbstractMediaManager";
-import { MediaFileRecord } from "../../../../models/MediaFile";
-export declare class ConvertType {
-    static MF2Item(mf: MediaFileRecord): MediaManagerItem;
-    static MF2Item(mf: MediaFileRecord[]): MediaManagerItem[];
-    static Item2MF(item: Partial<MediaManagerItem>): MediaFileRecord;
-    static Item2MF(items: Partial<MediaManagerItem>[]): MediaFileRecord[];
-}
--- libs/adminpanel/models/lib/group.d.ts
+++
@@ -1,9 +0,0 @@
-import { CreateUpdateConfig, FieldsModels } from "sails-adminpanel/interfaces/adminpanelConfig";
-export declare class GroupConfig {
-    static fields: FieldsModels;
-    static add(): CreateUpdateConfig;
-    static edit(): CreateUpdateConfig;
-    static list(): {
-        fields: FieldsModels;
-    };
-}
