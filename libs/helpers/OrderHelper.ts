ec38708e7f23904f7e96b5ddcb30a0f7c050d9c5
Ivan S <xziy88@gmail.com>
2026-04-28 16:52:18 +0700

    fix types for Order helper

--- libs/helpers/OrderHelper.ts
+++ libs/helpers/OrderHelper.ts
@@ -26,7 +26,7 @@ export type InitCheckout = {
   /**
    * Above the expenditure of bonuses on a check of a piece that will be shown to the user
    */
-  bonusBannerHTMLChunk: string
+  bonusBannerHTMLChunk: string | null
 
   /**
    *  the minimum time required to cook the dish. This value is added to the minimum delivery time to calculate the earliest possible time the order can be ready.
@@ -37,7 +37,7 @@ export type InitCheckout = {
    * Optional textual description of cooking time (e.g. "Prepared in 15–20 minutes").
    * Useful for displaying estimated preparation time to customers.
    */
-  cookingTimeDescription?: string;
+  cookingTimeDescription?: string | null;
 }
 export class OrderHelper {
   private constructor(orderId: string) {
@@ -55,8 +55,8 @@ export class OrderHelper {
       cookingTimeDescription: null
     }
     await emitter.emit('core:order-init-checkout', populatedOrder, initCheckout);
-    initCheckout.nonce = populatedOrder.nonce
-    initCheckout.bonusBannerHTMLChunk = await Settings.get("BONUS_BANNER_HTML_CHUNK")
+    initCheckout.nonce = populatedOrder.nonce as number
+    initCheckout.bonusBannerHTMLChunk = await Settings.get("BONUS_BANNER_HTML_CHUNK") as string
     return initCheckout;
   }
 
