1e7d7709af403cb299cf10f486ee197480e97ace
Ivan S <xziy88@gmail.com>
2026-04-30 09:20:29 +0700

    delte unused js files

--- adapters/delivery/default/defaultDelivery.d.ts
+++
@@ -1,7 +0,0 @@
-import Address from "../../../interfaces/Address";
-import { OrderRecord } from "../../../models/Order";
-import DeliveryAdapter, { Delivery } from "../DeliveryAdapter";
-export declare class DefaultDeliveryAdapter extends DeliveryAdapter {
-    checkAbility(address: Address): Promise<Delivery>;
-    calculate(order: OrderRecord): Promise<Delivery>;
-}
